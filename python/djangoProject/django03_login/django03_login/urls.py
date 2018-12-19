c9d0ef844ce7a11d543f38472e001a6f17762a04
zl03 <zl03.qq.com>
2018-12-19 21:00:44 +0800

    1219

--- python/djangoProject/django03_login/django03_login/urls.py
+++ python/djangoProject/django03_login/django03_login/urls.py
@@ -16,15 +16,20 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django03_login_app import views
-from django03_translate import views_translate
+from django03_translate import views as views_translate
 
 
 urlpatterns = [
 	# url(r'^admin/', include(admin.site.urls)),
 	url(r'login/', views.login),
 	url(r'register/', views.register),
-	url(r'register_handle/', views.register_handle),
-	url(r'login_handle/', views.login_handle),
+	url(r'register_handler/', views.register_handler),
+	url(r'login_handler/', views.login_handler),
 	url(r'translate/', views_translate.translateaction),
-	url(r'translate_submit/', views_translate.translate_handle)
+	url(r'translate_submit/', views_translate.translate_handle),
+	url(r'main/', views.mainpage),
+	url(r'after_register/', views.after_register)
+	#后期做成，在各个应用目录下新建urls.py文件，在这里调用就行
+	#url(r'',include(django03_translate.site.urls))
+
 	]
